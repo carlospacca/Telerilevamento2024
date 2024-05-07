2b075920dab6523ad4d838728e73d60fcdbce1de
Carlo Spacca <146854961+carlospacca@users.noreply.github.com>
2024-05-07 17:31:26 +0200

    Create 10_Latex.R

---
+++ 10_Latex.R
@@ -0,0 +1,25 @@
+# Latex è un programma che viene utilizzato per scrivere tesi e fare anche presentazioni, i matematici sono estremamente forti ad utilizzarlo
+
+# Documentclass permette di gestire la categoria del documento
+# Usepackage permette di inserire delle estenzioni su Overleaf e sul Latex
+# Title invece è il titolo del documento
+# Author è l'autore 
+
+# Con recompile avremo un' anteprima del lavoro
+
+# Possiamo togliere le funzioni o con un commento inserendo % prima della funzione o eliminando proprio la funzione
+
+# Begin
+# Si tratta di una funzione ultra generica che inizia un processo, nel nostro caso inizia un documento, ogni volta che c'è un begin dev'esserci un end
+
+# Con le section si crea l'indice, aggiungendo un asterisco dopo section si annulla il numero.
+
+# Per scrivere basta scrivere al di sotto delle section, per fare un nuovo paragrafo va lasciata una riga vuota, con \noindent si toglie lo spazio automatico
+
+# Per mettere il carattere a 12 a document class aggiungigamo [12pt]
+
+# Aggiungendo \tableofcontents sotto maketitle abbiamo un indice di tutti i section che abbiamo messo
+
+# Se in usepackage metto hyperreff metto i link interni al documento che dall'indice vanno ai paragrafi
+
+# \begin(equation)  inizia un equazione
